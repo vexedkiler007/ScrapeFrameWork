6070642f921a4fc3b00969046be79188f5839d38
Vexed <vexedkiller007@gmail.com>
2020-07-28 02:02:03 -0700

    Added TextDataBaseSQLalchemySaver and TextDataGinoSaver

--- Save.py
+++ Save.py
@@ -5,6 +5,8 @@ import aiohttp
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Table, MetaData, create_engine
+import gino
+from gino.schema import GinoSchemaVisitor
 
 
 class Saver(typing.Protocol):
@@ -36,15 +38,16 @@ class TextSaver(BaseSaver, Saver):
     async def save(self) -> None:
         async with AIOFile(f"{self.data.filename}.html", 'wb') as afp:
             await afp.write(self.data.bytes_)
+
             await afp.fsync()
 
 
-class ImageSaver(BaseSaver, Saver):
-    class ImageSaverData:
+class ImageVideoSaver(BaseSaver, Saver):
+    class ImageVideoSaverData:
         def __init__(self, list_urls: typing.List[str], list_filenames: typing.List[str]):
             self.list_urls = list_urls
             self.list_filenames = list_filenames
-    data_type = ImageSaverData
+    data_type = ImageVideoSaverData
 
     def __init__(self, data):
         super().__init__(data)
@@ -62,11 +65,13 @@ class ImageSaver(BaseSaver, Saver):
             await asyncio.gather(*coros)
 
 
+# have not tested
 class TextDataBaseGinoSaver(BaseSaver, Saver):
     class TextDataBaseSaverData:
-        def __init__(self, cols_name: typing.Tuple,data_kind: typing.Tuple, rows: typing.Tuple):
-            self.cols_name = cols_name
-            self.data_kind = data_kind
+        def __init__(self, engine_address: str,metadata: MetaData, table: Table, rows: typing.Tuple):
+            self.engine_address = engine_address
+            self.metadata = metadata
+            self.table = table  # the table must be built with the right metadata
             self.rows = rows
     data_type = TextDataBaseSaverData
 
@@ -76,7 +81,12 @@ class TextDataBaseGinoSaver(BaseSaver, Saver):
         # engine = sqlalchemy.create_engine("postgresql://postgres:password@localhost/postgres")
 
     async def save(self):
-        pass
+        engine = await gino.create_engine(self.data.engine_address)
+        await GinoSchemaVisitor(self.data.metadata).create_all(engine)
+        cols_names =
+        many_rows_format = [{key: val for key, val in zip(cols_names, row)} for row in self.data.rows]
+        conn = await engine.aquire()
+        await conn.execute(self.data.table.insert(), many_rows_format)
 
 
 class TextDataBaseSQLalchemySaver(BaseSaver, Saver):
@@ -103,8 +113,30 @@ class TextDataBaseSQLalchemySaver(BaseSaver, Saver):
         table = Table(*args)
         self.data.meta_data.create_all(self.data.sql_engine)
         many_rows_format = [{key: val for key, val in zip(cols_names, row)} for row in self.data.rows]
-        print(many_rows_format)
         conn = self.data.sql_engine.connect()
         conn.execute(table.insert(), many_rows_format)
+        conn.close()
+
+
+# this is very similar TextDataBaseSQLalchemySaver except it takes in a sqlalchemt table obj
+class TextDataBaseSQLalchemyTableSaver(BaseSaver, Saver):
+    class TextDataBaseSQLalchemyTableSaverData:
+        def __init__(self, sql_engine: create_engine, meta_data: MetaData, rows: list, table: Table):
+            self.sql_engine = sql_engine
+            self.meta_data = meta_data
+            self.rows = rows
+            self.table = table
+    data_type = TextDataBaseSQLalchemyTableSaverData
+
+    def __init__(self, data):
+        super().__init__(self)
+        self.data = data
+
+    async def save(self):
+        cols_names = [col.name for col in self.data.built_table.__dict__['columns']]
+        self.data.meta_data.create_all(self.data.sql_engine)
+        many_rows_format = [{key: val for key, val in zip(cols_names, row)} for row in self.data.rows]
+        conn = self.data.sql_engine.connect()
+        conn.execute(self.data.table.insert(), many_rows_format)
         conn.close()
 
